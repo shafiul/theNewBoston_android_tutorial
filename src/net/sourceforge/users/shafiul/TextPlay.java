a31f0e0981eaf589b5d125632d8cd8449e2d46b4
Shafiul Azam <ishafiul@gmail.com>
2012-02-16 04:02:14 +0600

    Tutorial 29

--- src/net/sourceforge/users/shafiul/TextPlay.java
+++ src/net/sourceforge/users/shafiul/TextPlay.java
@@ -3,6 +3,7 @@ package net.sourceforge.users.shafiul;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +22,7 @@ public class TextPlay extends Activity {
 		Button chkCmd = (Button) findViewById(R.id.bResults);
 		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		final EditText input = (EditText) findViewById(R.id.etCommands);
-		TextView display = (TextView) findViewById(R.id.tvResults);
+		final TextView display = (TextView) findViewById(R.id.tvResults);
 		
 		passTog.setOnClickListener(new View.OnClickListener() {
 			
@@ -40,10 +41,15 @@ public class TextPlay extends Activity {
 			public void onClick(View v) {
 				String check = input.getText().toString();
 				if(check.contentEquals("left")){
+					display.setGravity(Gravity.LEFT);
 					
 				}else if(check.contentEquals("center")){
+					display.setGravity(Gravity.CENTER);
 					
 				}else if(check.contentEquals("right")){
+					display.setGravity(Gravity.RIGHT);
+					
+				}else if(check.contentEquals("blue")){
 					
 				}
 				
