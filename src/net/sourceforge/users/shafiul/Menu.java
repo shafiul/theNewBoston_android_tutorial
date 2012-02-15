1a4dda4803ff4ab101e66eb092a67dc86ff8dd63
Shafiul Azam <ishafiul@gmail.com>
2012-02-15 23:20:07 +0600

    Tutorial 19

--- src/net/sourceforge/users/shafiul/Menu.java
+++ src/net/sourceforge/users/shafiul/Menu.java
@@ -3,11 +3,13 @@ package net.sourceforge.users.shafiul;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Menu extends ListActivity {
 	
-	String classes[] = {"startPoint", "Example1", "Example2", "Example3", "Example4", "Example5", "Example6"};
+	String classes[] = {"startPoint", "Example1", "Example2", "Example3", "Example4", 
+			"Example5", "Example6"};
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
@@ -19,6 +21,8 @@ public class Menu extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		setListAdapter(new ArrayAdapter<String>(Menu.this, android.R.layout.simple_list_item_1, classes));
 	}
 	
 	
