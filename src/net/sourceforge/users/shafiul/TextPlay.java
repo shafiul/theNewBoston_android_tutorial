c04eec25b7e6ab0b75dcb14366fce85f28710e6a
Shafiul Azam <ishafiul@gmail.com>
2012-02-16 03:32:21 +0600

    Tutorial 26

--- src/net/sourceforge/users/shafiul/TextPlay.java
+++ src/net/sourceforge/users/shafiul/TextPlay.java
@@ -2,6 +2,7 @@ package net.sourceforge.users.shafiul;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,9 +18,20 @@ public class TextPlay extends Activity {
 		setContentView(R.layout.text);
 		
 		Button chkCmd = (Button) findViewById(R.id.bResults);
-		ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
+		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		EditText input = (EditText) findViewById(R.id.etCommands);
 		TextView display = (TextView) findViewById(R.id.tvResults);
+		
+		passTog.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				if(passTog.isChecked()){
+					
+				}else{
+					
+				}
+			}
+		});
 	}
 
 	
