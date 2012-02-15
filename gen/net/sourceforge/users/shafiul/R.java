97201424471e28cd246f6f6f762cabc6c34fb6a4
Shafiul Azam <ishafiul@gmail.com>
2012-02-16 01:35:48 +0600

    Tutorial 22

--- gen/net/sourceforge/users/shafiul/R.java
+++ gen/net/sourceforge/users/shafiul/R.java
@@ -16,12 +16,16 @@ public final class R {
     }
     public static final class id {
         public static final int bAdd=0x7f060001;
+        public static final int bResults=0x7f060004;
         public static final int bSub=0x7f060002;
+        public static final int etCommands=0x7f060003;
         public static final int tvDisplay=0x7f060000;
+        public static final int tvResults=0x7f060005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int splash=0x7f030001;
+        public static final int text=0x7f030002;
     }
     public static final class raw {
         public static final int splashsound=0x7f040000;
