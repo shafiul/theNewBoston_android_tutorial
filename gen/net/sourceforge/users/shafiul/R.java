5b0597b8554e163f2cf10c8b70b0f18b335fe78a
Shafiul Azam <ishafiul@gmail.com>
2012-02-15 20:24:40 +0600

    Tutorial 11 (Failed to convert @drawable/splash_background into a drawable)

--- gen/net/sourceforge/users/shafiul/R.java
+++ gen/net/sourceforge/users/shafiul/R.java
@@ -12,17 +12,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int splash_background=0x7f020001;
+        public static final int splash_background_copy=0x7f020002;
     }
     public static final class id {
-        public static final int bAdd=0x7f050001;
-        public static final int bSub=0x7f050002;
-        public static final int tvDisplay=0x7f050000;
+        public static final int bAdd=0x7f060001;
+        public static final int bSub=0x7f060002;
+        public static final int tvDisplay=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int splash=0x7f030001;
+    }
+    public static final class raw {
+        public static final int splashsound=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
