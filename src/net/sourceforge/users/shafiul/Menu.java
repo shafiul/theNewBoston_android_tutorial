baeb8a60256350df164b1715a369f506998d68a1
Shafiul Azam <ishafiul@gmail.com>
2012-02-15 23:02:05 +0600

    Tutorial 18

---
+++ src/net/sourceforge/users/shafiul/Menu.java
@@ -0,0 +1,27 @@
+package net.sourceforge.users.shafiul;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class Menu extends ListActivity {
+	
+	String classes[] = {"startPoint", "Example1", "Example2", "Example3", "Example4", "Example5", "Example6"};
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+	
+
+}
