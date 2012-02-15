f6ca623efea75d6f1749f196fa71152134710ac2
Shafiul Azam <ishafiul@gmail.com>
2012-02-16 03:25:35 +0600

    Tutorial 25

---
+++ src/net/sourceforge/users/shafiul/TextPlay.java
@@ -0,0 +1,28 @@
+package net.sourceforge.users.shafiul;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class TextPlay extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		// our code
+		setContentView(R.layout.text);
+		
+		Button chkCmd = (Button) findViewById(R.id.bResults);
+		ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
+		EditText input = (EditText) findViewById(R.id.etCommands);
+		TextView display = (TextView) findViewById(R.id.tvResults);
+	}
+
+	
+	
+	
+}
