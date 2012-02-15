64971c9d7499bb7c999162bf4e49ac66807e93ab
Shafiul Azam <ishafiul@gmail.com>
2012-02-15 22:27:17 +0600

    Tutorial 17

--- src/net/sourceforge/users/shafiul/Splash.java
+++ src/net/sourceforge/users/shafiul/Splash.java
@@ -2,15 +2,22 @@ package net.sourceforge.users.shafiul;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class Splash extends Activity {
 
+	MediaPlayer ourSong;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+		
+		ourSong = MediaPlayer.create(Splash.this, R.raw.splashsound);
+		ourSong.start();
+		
 		Thread timer = new Thread(){
 			public void run(){
 				try{
@@ -31,6 +38,8 @@ public class Splash extends Activity {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
+		
+		ourSong.release();
 		finish();
 	}
 	
