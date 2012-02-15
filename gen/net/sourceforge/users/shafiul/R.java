4c4fbb2a823f0297b57b7f44056256249ec030c8
Shafiul Azam <ishafiul@gmail.com>
2012-02-15 20:45:58 +0600

    Tutorial 11 - fixed rendering background_splash.png related problems

--- gen/net/sourceforge/users/shafiul/R.java
+++ gen/net/sourceforge/users/shafiul/R.java
@@ -13,7 +13,6 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int splash_background=0x7f020001;
-        public static final int splash_background_copy=0x7f020002;
     }
     public static final class id {
         public static final int bAdd=0x7f060001;
