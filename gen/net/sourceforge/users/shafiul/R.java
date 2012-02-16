1dee907c37b279cd307d4866ec1bc8bb53011bb3
Shafiul Azam <ishafiul@gmail.com>
2012-02-16 14:00:27 +0600

    Tutorial 38

--- gen/net/sourceforge/users/shafiul/R.java
+++ gen/net/sourceforge/users/shafiul/R.java
@@ -17,25 +17,29 @@ public final class R {
     public static final class id {
         public static final int analogClock1=0x7f060007;
         public static final int bAdd=0x7f060009;
-        public static final int bResults=0x7f06000c;
+        public static final int bResults=0x7f06000f;
         public static final int bSentEmail=0x7f060006;
+        public static final int bSetWall=0x7f06000d;
         public static final int bSub=0x7f06000a;
         public static final int etAction=0x7f060004;
-        public static final int etCommands=0x7f06000b;
+        public static final int etCommands=0x7f06000e;
         public static final int etEmails=0x7f060000;
         public static final int etIntro=0x7f060001;
         public static final int etName=0x7f060002;
         public static final int etOutro=0x7f060005;
         public static final int etThings=0x7f060003;
-        public static final int tbPassword=0x7f06000d;
+        public static final int ibTakePic=0x7f06000c;
+        public static final int ivReturnedPic=0x7f06000b;
+        public static final int tbPassword=0x7f060010;
         public static final int tvDisplay=0x7f060008;
-        public static final int tvResults=0x7f06000e;
+        public static final int tvResults=0x7f060011;
     }
     public static final class layout {
         public static final int email=0x7f030000;
         public static final int main=0x7f030001;
-        public static final int splash=0x7f030002;
-        public static final int text=0x7f030003;
+        public static final int photo=0x7f030002;
+        public static final int splash=0x7f030003;
+        public static final int text=0x7f030004;
     }
     public static final class raw {
         public static final int splashsound=0x7f040000;
