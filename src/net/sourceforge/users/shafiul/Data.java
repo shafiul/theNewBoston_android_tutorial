28a903996b46614801cd439cc1da2b21dd863cec
Shafiul Azam <ishafiul@gmail.com>
2012-02-17 00:15:50 +0600

    Tutorial 46

--- src/net/sourceforge/users/shafiul/Data.java
+++ src/net/sourceforge/users/shafiul/Data.java
@@ -2,8 +2,17 @@ package net.sourceforge.users.shafiul;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Data extends Activity {
+public class Data extends Activity implements OnClickListener{
+	
+	Button start, startFor;
+	EditText sendET;
+	TextView gotAnswer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +20,23 @@ public class Data extends Activity {
 		super.onCreate(savedInstanceState);
 		// custom
 		setContentView(R.layout.get);
+		initialize();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		start = (Button) findViewById(R.id.bSA);
+		startFor = (Button) findViewById(R.id.bSAFR);
+		sendET = (EditText) findViewById(R.id.etSend);
+		gotAnswer = (TextView) findViewById(R.id.tvGot);
+		
+		start.setOnClickListener(this);
+		startFor.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/net/sourceforge/users/shafiul/OpenedClass.java
+++ src/net/sourceforge/users/shafiul/OpenedClass.java
@@ -2,13 +2,52 @@ package net.sourceforge.users.shafiul;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
 
-public class OpenedClass extends Activity {
+public class OpenedClass extends Activity implements OnClickListener, OnCheckedChangeListener {
+	
+	TextView question, test;
+	Button returnData;
+	RadioGroup selectionList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		// custom
+		setContentView(R.layout.send);
+		initialize();
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		question = (TextView) findViewById(R.id.tvQuestion);
+		test = (TextView) findViewById(R.id.tvText);
+		returnData = (Button) findViewById(R.id.bReturn);
+		returnData.setOnClickListener(this);
+		selectionList.setOnCheckedChangeListener(this);
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		switch(checkedId){
+		case R.id.rCrazy:
+			break;
+		case R.id.rSexy:
+			break;
+		case R.id.rBoth:
+			break;
+		}
+		
 	}
 
 }
