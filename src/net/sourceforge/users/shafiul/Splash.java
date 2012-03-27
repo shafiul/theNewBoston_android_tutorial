df58ab7df50cd2ec2caf4f1384d4547e4ea609dc
Shafiul Azam <ishafiul@gmail.com>
2012-03-28 01:48:07 +0600

    Tutorial 57

--- src/net/sourceforge/users/shafiul/Splash.java
+++ src/net/sourceforge/users/shafiul/Splash.java
@@ -2,35 +2,43 @@ package net.sourceforge.users.shafiul;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 public class Splash extends Activity {
 
 	MediaPlayer ourSong;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
-		
+
 		ourSong = MediaPlayer.create(Splash.this, R.raw.splashsound);
-		ourSong.start();
-		
-		Thread timer = new Thread(){
-			public void run(){
-				try{
+		SharedPreferences getPrefs = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		boolean music = getPrefs.getBoolean("checkbox", true);
+		if (music == true) {
+			ourSong.start();
+		}
+
+		Thread timer = new Thread() {
+			public void run() {
+				try {
 					sleep(1000);
-				}catch(InterruptedException e){
+				} catch (InterruptedException e) {
 					e.printStackTrace();
-				}finally{
-					Intent openStartingPoint = new Intent("net.sourceforge.users.shafiul.MENU");
+				} finally {
+					Intent openStartingPoint = new Intent(
+							"net.sourceforge.users.shafiul.MENU");
 					startActivity(openStartingPoint);
 				}
 			}
 		};
-		
+
 		timer.start();
 	}
 
@@ -38,11 +46,9 @@ public class Splash extends Activity {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
-		
+
 		ourSong.release();
 		finish();
 	}
-	
-	
-	
+
 }
