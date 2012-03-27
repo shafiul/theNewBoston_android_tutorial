73955988767754d53a21498ee31f04a4bf538696
Shafiul Azam <ishafiul@gmail.com>
2012-03-27 22:44:22 +0600

    Tutorial 52

--- src/net/sourceforge/users/shafiul/Menu.java
+++ src/net/sourceforge/users/shafiul/Menu.java
@@ -4,6 +4,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -47,5 +48,17 @@ public class Menu extends ListActivity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch(item.getItemId()){
+			case R.id.aboutus:
+				break;
+			case R.id.preferences:
+				break;
+		}
+		return false;
+	}
 
+	
 }
